515bc0f7fcc6ec937249d1c3fec285f568c3f102
Kunal <risk.always@gmail.com>
2020-09-24 19:45:23 +0530

    Props and State

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    perons:[
+      {
+        name:'Ricky',
+        age:36
+      },
+      {
+        name:'Khusi',
+        age:30
+      },
+      {
+        name:'Dimple',
+        age:30
+      },
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I am React APP!</h1>
+        <p>This is really Working for Me!!!!</p>
+        <button>Switch Name</button>
+        <Person name={this.state.perons[0].name} age={this.state.perons[0].age}/>
+        <Person name={this.state.perons[1].name} age={this.state.perons[1].age}>My Hobby: PUBG</Person>
+        <Person name={this.state.perons[2].name} age={this.state.perons[2].age}/>
       </div>
     );
+    //return React.createElement('div',{className:'App'},React.createElement('h1','','KUNAL'));
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = (props) =>{
+    console.log(props);
+    return (
+        <div>
+            <p>I'm a {props.name} and I am {props.age} years old! </p>
+            <b>{props.children}</b>
+        </div>
+    )
+}
+
+export default person; 
