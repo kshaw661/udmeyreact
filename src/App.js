1d3bc6c0cf3c485ea1f93a20d9329657e6551871
Kunal <risk.always@gmail.com>
2020-09-28 14:09:44 +0530

    Pass by refrence

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
@@ -20,15 +19,39 @@ class App extends Component {
       },
     ]
   }
+  switchNameHandler = (newName) =>{
+    this.setState({
+      perons:[
+        {
+          name:newName,
+          age:36
+        },
+        {
+          name:'Khusi',
+          age:30
+        },
+        {
+          name:'Dimple',
+          age:30
+        },
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I am React APP!</h1>
         <p>This is really Working for Me!!!!</p>
-        <button>Switch Name</button>
-        <Person name={this.state.perons[0].name} age={this.state.perons[0].age}/>
-        <Person name={this.state.perons[1].name} age={this.state.perons[1].age}>My Hobby: PUBG</Person>
-        <Person name={this.state.perons[2].name} age={this.state.perons[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this,'Tiger')}>Switch Name</button>
+        <Person click={()=>{this.switchNameHandler('KITIGER')}} 
+          name={this.state.perons[0].name} 
+          age={this.state.perons[0].age}/>
+        <Person 
+          name={this.state.perons[1].name} 
+          age={this.state.perons[1].age}>My Hobby: PUBG</Person>
+        <Person 
+          name={this.state.perons[2].name} 
+          age={this.state.perons[2].age}/>
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1','','KUNAL'));
