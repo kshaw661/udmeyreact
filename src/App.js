01c4cef0264dea62ca0046cef19529658c83a887
Kunal <risk.always@gmail.com>
2020-10-05 20:15:40 +0530

    Working With List and conditional Lists and output

--- src/App.js
+++ src/App.js
@@ -1,89 +1,100 @@
 import React, { Component } from 'react';
 import './App.css';
+import person from './Person/Person';
 import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    perons:[
+    perons: [
       {
-        name:'Ricky',
-        age:36
+        id:1,
+        name: 'Ricky',
+        age: 36
       },
       {
-        name:'Khusi',
-        age:30
+        id:2, 
+        name: 'Luka',
+        age: 36
       },
       {
-        name:'Dimple',
-        age:30
+        id:3,
+        name: 'Khusi',
+        age: 30
       },
-    ]
-  }
-  switchNameHandler = (newName) =>{
-    this.setState({
-      perons:[
-        {
-          name:newName,
-          age:36
-        },
-        {
-          name:'Khusi',
-          age:30
-        },
-        {
-          name:'Dimple',
-          age:30
-        },
-      ]
-    })
+      {
+        id:4,
+        name: 'Dimple',
+        age: 30
+      },
+    ],
+    showPerson: false
   }
 
-  nameChangedHandler = (event) => {
+  deletePersonHandler = (index) =>{
+    console.log(index);
+    //const persons = this.state.perons.slice();
+    const persons = [...this.state.perons]
+    persons.splice(index,1);
+    this.setState({
+      perons:persons
+    })
+  }
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.perons.findIndex(p=>{
+      return p.id === id
+    })
+    const person = {...this.state.perons[personIndex]}
+    person.name = event.target.value;
+    const persons = [...this.state.perons]
+    persons[personIndex] = person;
     this.setState(
       {
-        perons:[
-          {
-            name:'Nick',
-            age:16
-          },
-          {
-            name:event.target.value,
-            age:30
-          },
-          {
-            name:'Dimple',
-            age:30
-          },
-        ]
+        perons: persons
       }
     )
 
   }
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState(
+      {
+        showPerson: !doesShow
+      }
+    )
+  }
+
   render() {
     const style = {
-      backgroundColor:'white',
-      font:'inherit',
+      backgroundColor: 'white',
+      font: 'inherit',
       border: '1px solid blue',
-      padding:'8px',
-      cursor:'pointer'
+      padding: '8px',
+      cursor: 'pointer'
+    }
+    let persons = null;
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {this.state.perons.map((person,index) => {
+            return <Person 
+                      click={()=>this.deletePersonHandler(index)}
+                      name={person.name} 
+                      age={person.age}
+                      key={person.id}
+                      changed={(event)=>this.nameChangedHandler(event,person.id)}
+                    />
+          })}
+          
+        </div>
+      )
     }
     return (
       <div className="App">
         <h1>Hi, I am React APP!</h1>
         <p>This is really Working for Me!!!!</p>
-        <button style={style} onClick={this.switchNameHandler.bind(this,'Tiger')}>Switch Name</button>
-        <Person click={()=>{this.switchNameHandler('KITIGER')}} 
-          name={this.state.perons[0].name} 
-          age={this.state.perons[0].age}/>
-        <Person 
-          name={this.state.perons[1].name} 
-          age={this.state.perons[1].age}
-          changed={this.nameChangedHandler}
-          >My Hobby: PUBG</Person>
-        <Person 
-          name={this.state.perons[2].name} 
-          age={this.state.perons[2].age}
-          />
+        <button style={style} onClick={this.togglePersonHandler}>Toggel Persons</button>
+
+        {persons}
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1','','KUNAL'));
